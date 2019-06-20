c9791c969572682ec61cfe4a0d5a215ce325b5e2
maximuzzz <studentomaximo@gmail.com>
2019-06-19 23:36:31 -0700

    Resolved my first issue.

---
+++ Smack/Controller/ChatVC.swift
@@ -0,0 +1,22 @@
+//
+//  ChatVC.swift
+//  Smack
+//
+//  Created by Maksim Borodin on 6/18/19.
+//  Copyright © 2019 maksimus. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    //Outlets
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+    }
+  
+}
