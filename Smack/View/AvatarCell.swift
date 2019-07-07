b8d1353fbcf7a0281e2a3d4bab0c7fae52a926d9
maximuzzz <studentomaximo@gmail.com>
2019-07-07 14:18:07 -0700

    Lesson 13.

---
+++ Smack/View/AvatarCell.swift
@@ -0,0 +1,26 @@
+//
+//  AvatarCell.swift
+//  Smack
+//
+//  Created by Maksim Borodin on 7/7/19.
+//  Copyright © 2019 maksimus. All rights reserved.
+//
+
+import UIKit
+
+class AvatarCell: UICollectionViewCell {
+    
+    @IBOutlet weak var avatarImg: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setUpView()
+    }
+    
+    func setUpView() {
+        self.layer.backgroundColor = UIColor.lightGray.cgColor
+        self.layer.cornerRadius = 10
+        self.clipsToBounds = true
+    }
+    
+}
